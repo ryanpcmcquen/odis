df0c2dd2bfe952dc42bb01989adfb45d41292d58
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-05-04 06:35:10 -0700

    Make LibreJS compliant.
    Signed-off-by: Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>

--- odis.js
+++ odis.js
@@ -1,49 +1,50 @@
-/* odis v0.1.2 by ryanpcmcquen */
-/*jslint browser:true, white:true*/
+// @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2-or-later
+/* odis v1.0.0 by ryanpcmcquen */
+/*jslint browser:true*/
 /*global window*/
-
-(function() {
-  'use strict';
-  // we'll attach odis globally later
-  var odis = {
-    /* odis v0.1.2 by ryanpcmcquen */
-    debounce: function(func, delay) {
-      // set a reasonable timeout for debouncing
-      // (42 * 5), more on that later
-      delay = delay || 210;
-      var funcTimeoutId;
-      return function() {
-        // keep clearing until it stops being called
-        clearTimeout(funcTimeoutId);
-        funcTimeoutId = setTimeout(function() {
-          func.call();
-        }, delay);
-      };
-    },
-    throttle: function(func, delay) {
-      // nod to Douglas Adams  ;^)
-      delay = delay || 42;
-      var waiting = false,
-        funcTimeoutId;
-      return function() {
-        if (!waiting) {
-          // very similar to debounce, but 'waiting'
-          // allows execution while being called
-          waiting = true;
-          clearTimeout(funcTimeoutId);
-          funcTimeoutId = setTimeout(function() {
-            func.call();
-            waiting = false;
-          }, delay);
+(function () {
+    "use strict";
+    // We will attach odis globally later.
+    var odis = {
+        /* odis v1.0.0 by ryanpcmcquen */
+        debounce: function (func, delay) {
+            // Set a reasonable timeout for debouncing
+            // (42 * 5), more on that later.
+            delay = delay || 210;
+            var funcTimeoutId;
+            return function () {
+                // Keep clearing until it stops being called.
+                clearTimeout(funcTimeoutId);
+                funcTimeoutId = setTimeout(function () {
+                    func.call();
+                }, delay);
+            };
+        },
+        throttle: function (func, delay) {
+            // Nod to Douglas Adams.  ;^)
+            delay = delay || 42;
+            var waiting = false;
+            var funcTimeoutId;
+            return function () {
+                if (!waiting) {
+                    // Very similar to debounce, but 'waiting'
+                    // allows execution while being called.
+                    waiting = true;
+                    clearTimeout(funcTimeoutId);
+                    funcTimeoutId = setTimeout(function () {
+                        func.call();
+                        waiting = false;
+                    }, delay);
+                }
+            };
         }
-      };
-    }
-  };
-  // set up nice shortcuts because no one likes typing
-  odis.thro = odis.throttle;
-  odis.t = odis.thro;
-  odis.debo = odis.debounce;
-  odis.d = odis.debo;
-  // attach odis globally
-  window.odis = odis;
+    };
+    // Set up nice shortcuts because no one likes typing.
+    odis.thro = odis.throttle;
+    odis.t = odis.thro;
+    odis.debo = odis.debounce;
+    odis.d = odis.debo;
+    // Attach odis globally:
+    window.odis = odis;
 }());
+// @license-end
