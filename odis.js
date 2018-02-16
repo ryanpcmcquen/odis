8cbce2fa6f54d21187f3e87340aa55e99f08bfcd
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-02-16 15:04:45 -0800

    1.1.0

--- odis.js
+++ odis.js
@@ -1,5 +1,5 @@
 // @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2-or-later
-/* odis v1.0.0 by ryanpcmcquen */
+/* odis v1.1.0 by ryanpcmcquen */
 /*jslint browser:true*/
 /*global window*/
 (function () {
